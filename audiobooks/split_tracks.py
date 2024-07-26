daecc589c4587311ad6b1cf6e1c20775fef4322c
Zach Zahos <p.q.poiuy.qwert@gmail.com>
2024-07-26 14:46:02 -0400

    - Add better shifting - Add diffing to only split changed files - Remove existing files when overwriting

--- audiobooks/split_tracks.py
+++ audiobooks/split_tracks.py
@@ -1,6 +1,6 @@
-
-import sys, re, os, subprocess
-from typing import Callable
+from __future__ import annotations
+import sys, re, os, subprocess, hashlib, json
+from typing import Callable, TypedDict
 
 args = list(sys.argv)
 if args[0].endswith('.py'):
@@ -212,7 +212,7 @@ def only_keep(indexes, offset=1):
 def drop(indexes, offset=1):
     def drop_(tracks):
         ids = list((i if i > 0 else i + len(tracks)) for i in indexes)
-        return list(track for track in tracks if track['index']-offset not in ids)
+        return list(track for track in tracks if track['index']+offset not in ids)
     return drop_
 
 def trim(seconds):
@@ -252,25 +252,80 @@ def split_track(track_n: int, seconds: float, title1: str | None = None, title2:
         return tracks
     return split_track_
 
-def shift(track_n: int, seconds: float):
+def shift_forward(track_n: int, split_at_seconds: float):
     def shift_(tracks):
-        if seconds < 0:
-            tracks[track_n+1]['start'] += seconds
-            tracks[track_n]['end'] += seconds
-        else:
-            tracks[track_n-1]['end'] += seconds
-            tracks[track_n]['start'] += seconds
+        tracks[track_n-1]['end'] += split_at_seconds
+        tracks[track_n]['start'] += split_at_seconds
         return tracks
     return shift_
+
+def shift_back(track_n: int, split_at_seconds: float):
+    def shift_(tracks):
+        duration = tracks[track_n]['end'] - tracks[track_n]['start'] - split_at_seconds
+        tracks[track_n+1]['start'] -= duration
+        tracks[track_n]['end'] -= duration
+        return tracks
+    return shift_
+
+def rename(regex, repl: str | Callable[[re.Match[str]], str]):
+    def rename_(tracks):
+        regex_compiled = re.compile(regex)
+        for track in tracks:
+            track['title'] = regex_compiled.sub(repl, track['title'])
+        return tracks
+    return rename_
+
+def set_title(track_n: int, title: str):
+    def set_title_(tracks):
+        tracks[track_n]['title'] = title
+        return tracks
+    return set_title_
 
 counter = Counter()
 modifiers = [
     sort,
-    trim(3),
-    trim(-2),
-    shift(3, 1),
-    shift(28, 1),
-    only_keep([28,29])
+    trim(1.5),
+    set_title(0, 'Intro'),
+    set_title(-1, 'Outro'),
+    rename(r'([A-Z ]+):', lambda m: m.group(1).title() + ' -'),
+    rename(r'0+([1-9]+)', '\\1'),
+
+    shift_back(3, 5),
+    shift_back(4, secs(0, 26, 49)),
+    shift_back(5, secs(0, 11, 1)),
+    shift_back(6, secs(0, 33, 45)),
+    shift_back(7, secs(0, 22, 32)),
+    shift_back(8, secs(0, 44, 18)),
+    shift_back(9, secs(0, 4, 10)),
+    shift_back(10, secs(0, 10, 59)),
+    shift_back(11, secs(0, 26, 6)),
+    shift_back(12, secs(0, 10, 36)),
+    shift_back(13, secs(0, 28, 44)),
+    shift_back(14, secs(0, 18, 45)),
+    shift_back(15, secs(0, 36, 16)),
+
+    shift_back(17, 6),
+    shift_back(18, secs(0, 25, 6)),
+    shift_back(19, secs(0, 15, 36)),
+    shift_back(20, secs(0, 37, 43)),
+    shift_back(21, secs(0, 12, 57)),
+    shift_back(22, secs(0, 14, 52)),
+    shift_back(23, secs(0, 11, 28)),
+    shift_back(24, secs(0, 13, 42)),
+    shift_back(25, secs(0, 28, 0)),
+    shift_back(26, secs(0, 9, 1)),
+    shift_back(27, secs(0, 9, 25)),
+    shift_back(28, secs(0, 8, 30)),
+    shift_back(29, secs(0, 19, 4)),
+    shift_back(30, secs(0, 19, 30)),
+    shift_back(31, secs(0, 33, 44)),
+    shift_back(32, secs(0, 17, 13)),
+    shift_back(33, secs(0, 26, 1)),
+    shift_back(34, secs(0, 32, 17)),
+    shift_back(35, secs(0, 23, 9)),
+    shift_back(36, secs(0, 42, 46)),
+
+    shift_back(38, 5),
 ]
 
 if path.endswith('m4b'):
@@ -283,15 +338,93 @@ for modifier in modifiers:
 
 output_dir = os.path.dirname(path)
 
+
+BUF_SIZE = 65536
+def hash_file(path: str) -> hashlib._Hash:
+    sha1 = hashlib.sha1()
+    with open(path, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            sha1.update(data)
+    return sha1
+
+def hash_track(track: dict[str, str | int]) -> hashlib._Hash:
+    sha1 = hashlib.sha1()
+    for (key, value) in sorted(track.items(), key=lambda kv: kv[0]):
+        sha1.update(key.encode())
+        sha1.update(str(value).encode())
+    return sha1
+
+class Past(TypedDict):
+    class Input(TypedDict):
+        path: str
+        hash: str
+    class Track(TypedDict):
+        index: int
+        title: str
+        start: float
+        end: float
+    input: Input
+    tracks: list[Track]
+
+past: Past | None = None
+try:
+    past_path = os.path.join(output_dir, '.past')
+    if os.path.exists(past_path):
+        with open(past_path, 'r') as f:
+            past = json.load(f)
+except:
+    print("INFO: Couldn't load `.past` file")
+    pass
+
+input_changed = True
+input_hash = hash_file(path)
+if past is not None:
+    input_changed = input_hash.hexdigest() != past['input']['hash']
+
 zero_pad = max(len(str(len(tracks))), 2)
 for track in tracks:
-    command = ['ffmpeg', '-ss', '%d' % track['start'], '-i', path, '-vn', '-c:a', 'libmp3lame', '-q:a', '0']
+    output_name = ('%%0%dd' % zero_pad + ' - %s.mp3') % (track['index'] + 1, track['title'])
+    output_path = os.path.join(output_dir, output_name)
+
+    track_changed = True
+    if os.path.exists(output_path) and past is not None:
+        past_track: Past.Track | None = next(filter(lambda t: t['index'] == track['index'], past['tracks']))
+        if past_track is not None:
+            track_hash = hash_track(track)
+            past_hash = hash_track(past_track)
+            if track_hash.digest() == past_hash.digest():
+                track_changed = False
+
+    if not input_changed and not track_changed:
+        print(f"\t'{output_name}' unchanged")
+        continue
+
+    command = ['ffmpeg', '-ss', '%.2f' % track['start'], '-i', path, '-vn', '-c:a', 'libmp3lame', '-q:a', '0']
     if track['end'] != None:
-        command.extend(['-to', '%d' % (track['end'] - track['start'])])
-    command.append(os.path.join(output_dir, ('%%0%dd' % zero_pad + ' - %s.mp3') % (track['index'] + 1, track['title'])))
+        command.extend(['-to', '%.2f' % (track['end'] - track['start'])])
+    command.append(output_path)
     print(' '.join(command))
     if execute:
+        if os.path.exists(output_path):
+            os.unlink(output_path)
         print(subprocess.run(command))
 
 if not execute:
     print("WARNING: This was only a dry run, use the '-e' flag to execute")
+else:
+    past = {
+        'input': {
+            'path': path,
+            'hash': input_hash.hexdigest()
+        },
+        'tracks': tracks
+    }
+    try:
+        with open(past_path, 'w') as f:
+            json.dump(past, f, indent=2)
+    except:
+        print("INFO: Couldn't save `.past` file")
+        pass
